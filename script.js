56cdeb75c02db023b134257238956a53ee6d853d
Leanmucho <acunaleandro3@gmail.com>
2022-03-16 11:31:12 -0300

    Add music and datails

---
+++ script.js
@@ -0,0 +1,17 @@
+let boton = document.getElementById("boton");
+let v = document.getElementsByTagName("audio") [0];
+let sound = false
+
+boton.addEventListener("click", function(){
+    if (!sound) {
+      v.play();
+      this.innerHTML = "Pause";
+      sound = true;
+      document.getElementById("imagen").classList.add("App-logo")
+     } else {
+      v.pause();
+      this.innerHTML = "Play";
+      sound = false;
+      document.getElementById("imagen").classList.remove("App-logo")
+     } 
+   });
